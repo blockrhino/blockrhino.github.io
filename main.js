34d132ce40cdaa7877b88890cb19aa9329a899b1
blockrhino <rhinohanlon@gmail.com>
2017-01-12 10:29:10 -0600

    d

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-var userIp = $.getJSON('//api.ipify.org?format=jsonp&callback=?', function(data) {
+$.getJSON('//api.ipify.org?format=jsonp&callback=?', function(data) {
   console.log(JSON.stringify(data, null, 2));
-  
-  var myName = $.getJSON();
+
 });
 var red = [0, 100, 63];
 var orange = [40, 100, 60];
@@ -9,7 +8,10 @@ var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 
+var myName = $.getJSON('//api.ipify.org?format=jsonp&callback=?', function(data) {
+  console.log(JSON.stringify(data, null, 2));
 
+});
 letterColors = [red, orange, green, blue, purple];
 
   bubbleShape = "circle";
