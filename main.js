ec35993521aa007029b29e656565ccccf8703d4d
blockrhino <rhinohanlon@gmail.com>
2017-01-12 10:11:59 -0600

    hgnf

--- main.js
+++ main.js
@@ -5,14 +5,14 @@
 </script>
 
 <script type="application/javascript" src="http://www.telize.com/jsonip?callback=getip"></script>
-ip = getip()
+
 var red = [0, 100, 63];
 var orange = [40, 100, 60];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 
-var myName = "Rian O."
+var myName = getip(json);
 
 letterColors = [red, orange, green, blue, purple];
 
