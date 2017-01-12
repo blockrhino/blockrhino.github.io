9ea50b89af44991110c1579bd78194b2d0db026b
blockrhino <rhinohanlon@gmail.com>
2017-01-12 10:38:53 -0600

    tr

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-$.getJSON('//api.ipify.org?format=jsonp&callback=?', function(data) {
-  console.log(JSON.stringify(data, null, 2));
 
-});
 var red = [0, 100, 63];
 var orange = [40, 100, 60];
 var green = [75, 100, 40];
@@ -9,7 +6,7 @@ var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 
 var myName = $.getJSON('//api.ipify.org?format=jsonp&callback=?', function(data) {
-  return (JSON.stringify(data, null, 2));
+  console.log(JSON.stringify(data, null, 2));
 
 });
 letterColors = [red, orange, green, blue, purple];
