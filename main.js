420e3a0933d583fc49d715147f218614e55c5bd4
blockrhino <rhinohanlon@gmail.com>
2017-01-11 23:20:38 -0600

    test

--- main.js
+++ main.js
@@ -1,4 +1,70 @@
-var myName = "Rian";
+<html><head>
+    <style>
+      html, body, div, span, applet, object, iframe,
+      h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+      a, abbr, acronym, address, big, cite, code,
+      del, dfn, em, img, ins, kbd, q, s, samp,
+      small, strike, strong, sub, sup, tt, var,
+      b, u, i, center,
+      dl, dt, dd, ol, ul, li,
+      fieldset, form, label, legend,
+      table, caption, tbody, tfoot, thead, tr, th, td,
+      article, aside, canvas, details, embed,
+      figure, figcaption, footer, header, hgroup,
+      menu, nav, output, ruby, section, summary,
+      time, mark, audio, video {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 100%;
+        font: inherit;
+        vertical-align: baseline;
+      }
+      html, body, iframe {
+        height: 100%;
+      }
+      /* HTML5 display-role reset for older browsers */
+      article, aside, details, figcaption, figure,
+      footer, header, hgroup, menu, nav, section {
+        display: block;
+      }
+      body {
+        line-height: 1;
+      }
+      ol, ul {
+        list-style: none;
+      }
+      blockquote, q {
+        quotes: none;
+      }
+      blockquote:before, blockquote:after,
+      q:before, q:after {
+        content: '';
+        content: none;
+      }
+      table {
+        border-collapse: collapse;
+        border-spacing: 0;
+      }
+    </style>
+  </head>
+  <body>
+    <script type="text/javascript" src="stuff_wrapper.js?v1.0"></script>
+
+
+<iframe width="100%" height="100%"></iframe></body></html>
+<script type="text/javascript" src="stuff_wrapper.js?v1.0"></script>
+<iframe width="100%" height="100%"></iframe>
+<html><head><script></script>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script><script src="/assets/jquery.expect.js"></script><script src="/assets/web_libs.js?v5"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="https://s3.amazonaws.com/codecademy-content/courses/hour-of-code/js/alphabet.js"></script>
+  </head>
+  <body>
+    <canvas id="myCanvas" height="500" width="1000"></canvas>
+    <script type="text/javascript" src="https://s3.amazonaws.com/codecademy-content/courses/hour-of-code/js/bubbles.js"></script>
+    <script type="text/javascript" defer="">var myName = "Codecademy";
 
 var red = [0, 100, 63];
 var orange = [40, 100, 60];
@@ -18,4 +84,6 @@ else
     bubbleShape = 'circle';
 }
 
-bounceBubbles();
+bounceBubbles();</script>
+
+</body></html>
