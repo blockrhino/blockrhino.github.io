dca1f49173f0e2b68a686528ee4484bf36deaa40
blockrhino <rhinohanlon@gmail.com>
2017-01-11 23:34:28 -0600

    a

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 
-var myName = "Shane Lee O'Shea";
+var myName = "Rian";
 
 
 letterColors = [red, orange, green, blue, purple];
