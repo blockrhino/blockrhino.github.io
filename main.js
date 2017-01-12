485918f564e4524d87b79fb74bd481f7436ca65c
blockrhino <rhinohanlon@gmail.com>
2017-01-11 23:33:28 -0600

    s

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 
-var myName = "Shane Lee O'Shea"; //"Shane Lee O'Shea used as example
- // variables can be changed accordingly
+var myName = "Shane Lee O'Shea";
 
 
 letterColors = [red, orange, green, blue, purple];
