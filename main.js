62e4add78f11b52c086113dcf224d9e4ae132d61
blockrhino <rhinohanlon@gmail.com>
2017-01-12 00:41:26 -0600

    g

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 
 var myName = "Rian O."
-
+function check() {
+    document.getElementById("switch").checked = true;
+}
 letterColors = [red, orange, green, blue, purple];
-if (10 > 3) {
+if check() = true{
     bubbleShape = "circle";
 }
 else {
